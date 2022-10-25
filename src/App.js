3f8f520945aec74df42c59da2a1c445c68787305
Ali Raza Dar <dar.ali1993@gmail.com>
2022-10-25 14:27:10 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import './fonts/norwester.ttf'
 import {Routes, Route} from "react-router-dom"
+import { BrowserRouter as Switch, Link } from "react-router-dom"
 // import Navbar from './navbar'
 import Navbar from './Nav/Navbar';
 import Footer from './footer';
